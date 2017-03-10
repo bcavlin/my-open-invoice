d5e2309588a20f38b026aeb65c679673745b9e6f
Branislav Cavlin <bcavlin@gmail.com>
2017-03-09 23:34:52 -0500

    initial release

---
+++ invoice-server/src/main/java/com/bgh/myopeninvoice/InvoiceServerApplication.java
@@ -0,0 +1,12 @@
+package com.bgh.myopeninvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceServerApplication.class, args);
+	}
+}
