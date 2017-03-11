18720be1b71baef28b1472bd00721e83eed39abf
Branislav Cavlin <bcavlin@gmail.com>
2017-03-10 23:37:37 -0500

    updates

--- invoice-server/src/main/java/com/bgh/myopeninvoice/InvoiceServerApplication.java
+++ invoice-server/src/main/java/com/bgh/myopeninvoice/InvoiceServerApplication.java
@@ -2,11 +2,13 @@ package com.bgh.myopeninvoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:db.properties"})
 public class InvoiceServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InvoiceServerApplication.class, args);
+        SpringApplication.run(InvoiceServerApplication.class, args);
 	}
 }
