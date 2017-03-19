ebdf11b71be6ca23f41dd75f39dc9c501ec0eeb5
Branislav Cavlin <bcavlin@gmail.com>
2017-03-19 11:48:46 -0400

    update

--- invoice-db/src/main/java/com/bgh/myopeninvoice/db/dao/TaxRepository.java
+++ invoice-db/src/main/java/com/bgh/myopeninvoice/db/dao/TaxRepository.java
@@ -1,11 +1,12 @@
 package com.bgh.myopeninvoice.db.dao;
 
 import com.bgh.myopeninvoice.db.model.TaxEntity;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by bcavlin on 14/03/17.
  */
-public interface TaxRepository extends PagingAndSortingRepository<TaxEntity, Integer> {
+public interface TaxRepository extends PagingAndSortingRepository<TaxEntity, Integer>, QueryDslPredicateExecutor<TaxEntity> {
 
 }
--- invoice-server/src/main/java/com/bgh/myopeninvoice/jsfbeans/TaxExtityLazyModel.java
+++ invoice-server/src/main/java/com/bgh/myopeninvoice/jsfbeans/TaxExtityLazyModel.java
@@ -1,11 +1,16 @@
 package com.bgh.myopeninvoice.jsfbeans;
 
 import com.bgh.myopeninvoice.db.dao.TaxRepository;
+import com.bgh.myopeninvoice.db.model.QTaxEntity;
 import com.bgh.myopeninvoice.db.model.TaxEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +20,8 @@ import java.util.Map;
  * Created by bcavlin on 17/03/17.
  */
 public class TaxExtityLazyModel extends LazyDataModel<TaxEntity> {
+
+    private static Logger logger = LoggerFactory.getLogger(TaxExtityLazyModel.class);
 
     private List<TaxEntity> taxEntityList;
 
@@ -29,7 +36,53 @@ public class TaxExtityLazyModel extends LazyDataModel<TaxEntity> {
 
         taxEntityList = new ArrayList<>();
 
-        final Page<TaxEntity> taxEntityPage = taxRepository.findAll(new PageRequest(first * pageSize, pageSize));
+        PageRequest pageRequest = new PageRequest(first * pageSize, pageSize);
+
+        Sort.Direction direction = null;
+        if (sortOrder == SortOrder.ASCENDING) {
+            direction = Sort.Direction.ASC;
+        } else if (sortOrder == SortOrder.DESCENDING) {
+            direction = Sort.Direction.DESC;
+        }
+        try {
+            if (direction != null && sortField != null) {
+                pageRequest = new PageRequest(
+                        first * pageSize,
+                        pageSize,
+                        new Sort(new Sort.Order(direction, sortField)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        BooleanExpression predicate = null;
+
+        if (filters != null && !filters.isEmpty()) {
+//            logger.info("filtering...");
+            for (Map.Entry<String, Object> stringObjectEntry : filters.entrySet()) {
+                BooleanExpression temp = null;
+
+                if (QTaxEntity.taxEntity.identifier.toString().equalsIgnoreCase("taxEntity." + stringObjectEntry.getKey())) {
+                    temp = QTaxEntity.taxEntity.identifier.containsIgnoreCase(stringObjectEntry.getValue().toString());
+                }
+
+                if (temp != null) {
+                    if (predicate == null) {
+                        predicate = temp;
+                    } else {
+                        predicate.and(temp);
+                    }
+                }
+            }
+        }
+
+        Page<TaxEntity> taxEntityPage = null;
+
+        if (predicate == null) {
+            taxEntityPage = taxRepository.findAll(pageRequest);
+        } else {
+            taxEntityPage = taxRepository.findAll(predicate, pageRequest);
+        }
 
         setRowCount((int) taxEntityPage.getTotalElements());
         setPageSize(getPageSize());
