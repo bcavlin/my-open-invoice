40796362f0b056360ec14d34b454f7b36052453b
Branislav Cavlin <bcavlin@gmail.com>
2017-04-10 21:23:22 -0400

    update

--- invoice-server/src/main/java/com/bgh/myopeninvoice/jsfbeans/InvoiceBean.java
+++ invoice-server/src/main/java/com/bgh/myopeninvoice/jsfbeans/InvoiceBean.java
@@ -17,8 +17,10 @@
 package com.bgh.myopeninvoice.jsfbeans;
 
 import com.bgh.myopeninvoice.db.dao.InvoiceDAO;
-import com.bgh.myopeninvoice.db.model.InvoiceEntity;
+import com.bgh.myopeninvoice.db.model.*;
 import com.bgh.myopeninvoice.jsfbeans.model.InvoiceEntityLazyModel;
+import com.google.common.collect.Lists;
+import com.querydsl.core.types.Predicate;
 import org.primefaces.model.LazyDataModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +32,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
 
 /**
  * Created by bcavlin on 17/03/17.
@@ -41,31 +45,45 @@ public class InvoiceBean implements Serializable {
 
     private static Logger logger = LoggerFactory.getLogger(InvoiceBean.class);
 
-    @Autowired
     private InvoiceDAO invoiceDAO;
 
     private LazyDataModel<InvoiceEntity> invoiceEntityLazyDataModel;
+
+    private Collection<CompanyContactEntity> companyContactEntityCollectionForSelection;
+
+    private Collection<CompaniesEntity> companiesEntityCollectionForSelection;
+
+    private Collection<TaxEntity> taxEntityCollectionForSelection;
 
     private InvoiceEntity selectedInvoiceEntity;
 
     private int pageSize = 20;
 
+    @Autowired
+    public InvoiceBean(InvoiceDAO invoiceDAO) {
+        this.invoiceDAO = invoiceDAO;
+    }
+
     @PostConstruct
     private void init() {
         logger.info("Initializing entries");
         invoiceEntityLazyDataModel = new InvoiceEntityLazyModel(invoiceDAO);
+        Predicate p = QCompanyContactEntity.companyContactEntity.companiesByCompanyId.ownedByMe.isTrue();
+        companyContactEntityCollectionForSelection = Lists.newArrayList(invoiceDAO.getCompanyContactRepository().findAll(p));
+        Predicate p1 = QCompaniesEntity.companiesEntity.ownedByMe.isFalse();
+        companiesEntityCollectionForSelection = Lists.newArrayList(invoiceDAO.getCompaniesRepository().findAll(p1));
+
+        taxEntityCollectionForSelection = Lists.newArrayList(invoiceDAO.getTaxRepository().findAll());
     }
 
     private void refresh() {
         logger.info("Loading entries");
-        if (selectedInvoiceEntity != null) {
-            selectedInvoiceEntity = invoiceDAO.getInvoiceRepository().findOne(selectedInvoiceEntity.getInvoiceId());
-        }
     }
 
     public void newEntryListener(ActionEvent event) {
         logger.info("Creating new entity");
         selectedInvoiceEntity = new InvoiceEntity();
+        selectedInvoiceEntity.setCreatedDate(new Date());
     }
 
     public void ajaxChangeRowListener() {
@@ -78,6 +96,25 @@ public class InvoiceBean implements Serializable {
 
     public void deleteEntryListener(ActionEvent event) {
 
+    }
+
+    public void updateSelectionFromTo() {
+        if (selectedInvoiceEntity != null
+                && selectedInvoiceEntity.getCompanyContactFrom() != null
+                && selectedInvoiceEntity.getCompanyTo() != null
+                ) {
+            if(selectedInvoiceEntity.getCompanyContactByCompanyContactFrom()==null || selectedInvoiceEntity.getCompanyContactByCompanyContactFrom().getCompanyContactId()!=selectedInvoiceEntity.getCompanyContactFrom()) {
+                selectedInvoiceEntity.setCompanyContactByCompanyContactFrom(invoiceDAO.getCompanyContactRepository().findOne(selectedInvoiceEntity.getCompanyContactFrom()));
+                selectedInvoiceEntity.setCompaniesByCompanyTo(invoiceDAO.getCompaniesRepository().findOne(selectedInvoiceEntity.getCompanyTo()));
+                final ContractsEntity validContract = selectedInvoiceEntity.getCompanyContactByCompanyContactFrom().findValidContract(selectedInvoiceEntity.getCompaniesByCompanyTo());
+                if (validContract != null) {
+                    selectedInvoiceEntity.setRate(validContract.getRate());
+                    selectedInvoiceEntity.setRateUnit(validContract.getRateUnit());
+                    selectedInvoiceEntity.setCcy(validContract.getCcy());
+                    selectedInvoiceEntity.setCurrencyByCcy(validContract.getCurrencyByCcy());
+                }
+            }
+        }
     }
 
     public int getPageSize() {
@@ -102,5 +139,29 @@ public class InvoiceBean implements Serializable {
 
     public void setSelectedInvoiceEntity(InvoiceEntity selectedInvoiceEntity) {
         this.selectedInvoiceEntity = selectedInvoiceEntity;
+    }
+
+    public Collection<CompanyContactEntity> getCompanyContactEntityCollectionForSelection() {
+        return companyContactEntityCollectionForSelection;
+    }
+
+    public void setCompanyContactEntityCollectionForSelection(Collection<CompanyContactEntity> companyContactEntityCollectionForSelection) {
+        this.companyContactEntityCollectionForSelection = companyContactEntityCollectionForSelection;
+    }
+
+    public Collection<CompaniesEntity> getCompaniesEntityCollectionForSelection() {
+        return companiesEntityCollectionForSelection;
+    }
+
+    public void setCompaniesEntityCollectionForSelection(Collection<CompaniesEntity> companiesEntityCollectionForSelection) {
+        this.companiesEntityCollectionForSelection = companiesEntityCollectionForSelection;
+    }
+
+    public Collection<TaxEntity> getTaxEntityCollectionForSelection() {
+        return taxEntityCollectionForSelection;
+    }
+
+    public void setTaxEntityCollectionForSelection(Collection<TaxEntity> taxEntityCollectionForSelection) {
+        this.taxEntityCollectionForSelection = taxEntityCollectionForSelection;
     }
 }
