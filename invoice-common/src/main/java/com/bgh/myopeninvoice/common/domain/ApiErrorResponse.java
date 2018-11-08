280d137c5c9df50748d10a63080264c1ff6c407b
Branislav Cavlin <bcavlin@gmail.com>
2018-11-07 22:51:31 -0500

    Update

---
+++ invoice-common/src/main/java/com/bgh/myopeninvoice/common/domain/ApiErrorResponse.java
@@ -0,0 +1,12 @@
+package com.bgh.myopeninvoice.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiErrorResponse {
+    private Integer status;
+    private Integer code;
+    private String message;
+}
