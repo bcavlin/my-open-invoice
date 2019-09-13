b2288dbf00bf16548c890dec96ddc38322e736aa
Branislav Cavlin <bcavlin@gmail.com>
2019-09-12 23:00:55 -0400

    Bugfixes

--- invoice-common/src/main/java/com/bgh/myopeninvoice/common/controller/GlobalExceptionHandler.java
+++ invoice-common/src/main/java/com/bgh/myopeninvoice/common/controller/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.bgh.myopeninvoice.common.util.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,13 @@ public class GlobalExceptionHandler {
   ResponseEntity<?> handleThrowableException(Throwable ex) {
     return Utils.getErrorResponse(
         String.class, ex, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler({AuthenticationException.class})
+  @ResponseBody
+  ResponseEntity<?> handleAuthorizationException(Throwable ex) {
+    return Utils.getErrorResponse(
+            String.class, ex, ex.getMessage(), HttpStatus.UNAUTHORIZED);
   }
 
   @ExceptionHandler({NoHandlerFoundException.class})
